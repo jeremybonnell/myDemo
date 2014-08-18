dd1e96a48a77cc95e6f8d0d59eca0aaf50d0b45f
jeremybonnell <jeremy.bonnell@progfinance.com>
2014-08-18 16:17:34 -0600

    reverted back to table. Replace deprecated (itemViewContainer -> childViewContainer)

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -5,15 +5,15 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
 
     // Define and instantiate the view for each individual Model within a CollectionView
-    List.Contact = Marionette.ItemView.extend({ tagName: "li", template: "#contact-list-item" });
+    List.Contact = Marionette.ItemView.extend({ tagName: "tr", template: "#contact-list-item" });
 
     // Define and Instantiate a Marionette CollectionView object
     // which contain the objects of type List.Contact
     List.Contacts = Marionette.CompositeView.extend({
-        tagName: "div",                       //surround with <ul/>, Our template is of tag type <li/>
-        //className: "table table-hover",
-        template: "#contact-list-p",
-        childView: List.Contact,                //itemView: List.Contact (DEPRECATED: itemView)
-        childViewContainer: "ul"                //itemViewContainer: List.Contact (DEPRECATED: childViewContainer)
+        tagName: "table",                       //surround with <ul/>, Our template is of tag type <li/>
+        className: "table table-hover",
+        template: "#contact-list",
+        childView: List.Contact,                // (DEPRECATED: itemView), use childView
+        childViewContainer: "tbody"             // (DEPRECATED: itemViewContainer), use childViewContainer
     });
 });
