49d9ed455fa3b3a04603a1993c9379ff8a829721
jeremybonnell <jeremy.bonnell@progfinance.com>
2014-08-18 16:09:48 -0600

    Try exercise, experiment with CompositeView.
    itemViewContainer deprecated:
    use: childViewContainer.

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -5,15 +5,15 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
 
     // Define and instantiate the view for each individual Model within a CollectionView
-    List.Contact = Marionette.ItemView.extend({ tagName: "tr", template: "#contact-list-item" });
+    List.Contact = Marionette.ItemView.extend({ tagName: "li", template: "#contact-list-item" });
 
     // Define and Instantiate a Marionette CollectionView object
     // which contain the objects of type List.Contact
     List.Contacts = Marionette.CompositeView.extend({
-        tagName: "table",                       //surround with <ul/>, Our template is of tag type <li/>
-        className: "table table-hover",
-        template: "#contact-list",
+        tagName: "div",                       //surround with <ul/>, Our template is of tag type <li/>
+        //className: "table table-hover",
+        template: "#contact-list-p",
         childView: List.Contact,                //itemView: List.Contact (DEPRECATED: itemView)
-        itemViewContainer: "tbody"
+        childViewContainer: "ul"                //itemViewContainer: List.Contact (DEPRECATED: childViewContainer)
     });
 });
