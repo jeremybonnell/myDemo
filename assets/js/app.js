b507a63e625280eee74cba9c14308f565a900bd0
jeremybonnell <jeremy.bonnell@progfinance.com>
2014-08-20 17:18:00 -0600

    Add routing and history, have index.html with #contacts.

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,19 @@ ContactManager.addRegions({
 //--------------------Triggered by calling ContactManager.start(); in index.html
 // Create On-Start method which
 // Defines the events to occur when the application starts, using our pre-defined types.
-ContactManager.on("start", function () {
-    ContactManager.ContactsApp.List.Controller.listContacts();
+// Initializing Backbone history is REQUIRED FOR ROUTING!!!
+// Could have actually place the guts of this callback function into the one above.
+ContactManager.on("start", function() {
+    if (Backbone.history) {
+        Backbone.history.start();
+    }
+
+//    Here’s what we want to do: if the user comes to our app at the root URL, let’s redirect him to “#contacts”. The basic way of accomplishing this would be:
+    if(Backbone.history.fragment === "") {      // triple === in javascript has no type conversion before evaluating. but essentially the same as ==
+        Backbone.history.navigate("contacts");
+        ContactManager.ContactsApp.List.Controller.listContacts();
+
+//        // As a sideNote: Could also use the following line of code instead of the 2 preceding lines...Bad practice though I guess.
+//        Backbone.history.navigate("contacts", {trigger: true});
+    }
 });
---
+++ assets/js/apps/contacts/contacts_app.js
@@ -0,0 +1,37 @@
+/**
+ * Created by jeremy.bonnell on 8/20/2014.
+ */
+
+ContactManager.module("ContactsApp", function(ContactsApp, ContactManager, Backbone, Marionette, $, _){
+    ContactsApp.Router = Marionette.AppRouter.extend({
+        appRoutes: {
+
+            // Any URL with the fragment "contacts", will get routed to the "listContacts" callback function of ContactsApps module (in list_controller.js)
+            // Above stmt may or may not have any merit. it is my assumption.
+
+//            Note that the callback function (e.g. listContacts) specified in the appRoutes object above must exist in the router’s controller.
+//            In other words, all the callbacks used in the appRoutes object must be located in our API object.
+            "contacts": "listContacts"
+        }
+    });
+
+    var API = {
+        listContacts: function(){
+            //console.log("route to list contacts was triggered");
+            ContactsApp.List.Controller.listContacts();
+        }
+    };
+
+//    why are we listening for the “initialize:after” or [on("start"] rather event in other circumstances, instead of using addInitializer?
+//    Execution order. We can add initializers with calls to addInitializer, and the provided functions will be executed
+//    when the application is running. Then, once all initializers have been run, the [on("start"] event is triggered.
+
+//    the addInitializer method will execute the provided function when the app is running. This means
+//    that if the app isn’t yet running, it will wait until the app has started before running the code;
+//    but if the app is already running by the time you call addInitializer, the function will be executed immediately.
+    ContactManager.addInitializer(function(){
+        new ContactsApp.Router({
+            controller: API
+        });
+    });
+});
